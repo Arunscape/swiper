2d678e14354f9070ec25173f1fc420859847afcf
Arunscape <8227297+Arunscape@users.noreply.github.com>
2023-01-25 00:23:24 -0700

    😩

--- swiper/main.py
+++ swiper/main.py
@@ -1,115 +1,132 @@
-import inquirer
-
 # from ppadb.client_async import ClientAsync as AdbClient
-from ppadb.client import Client as AdbClient
-import uiautomator2 as u2
 import yaml
 import time
+import sys
+import requests
+import json
 
 
-# def click_more_info(d):
-#     if d(className="android.widget.ImageView", resourceId="com.tinder:id/recsDetailInfo").wait.exists(timeout=100):
-#         d(className="android.widget.ImageView", resourceId="com.tinder:id/recsDetailInfo").click()
+class User:
+    def __init__(self, data: dict):
+        self.data = data
 
-# def get_bio(d):
-#     return d(className="android.widget.TextView", resourceId="com.tinder:id/text_view_bio").info["text"]
+    def bio(self) -> str:
+        return self.data["user"]["bio"]
+    def name(self) -> str:
+        return self.data["user"]["name"]
 
-# def has_blacklisted_lifestyle(d):
-#     lifestyle = d(className="android.view.ViewGroup", resourceId="com.tinder:id/chip_group_descriptors")
-#     for l in blacklisted_lifestyles:
-#         if lifestyle.child(text=l, className="android.view.View").wait.exists(timeout=1):
-#             print(f"Rejected because {l}")
-#             return True
-#     return False
+    def interests(self) -> list[str]:
+        x = self.data.get("experiment_info", {}).get("user_interests", {}).get("selected_interests", [])
+        return [i["name"] for i in x]
+    
+    def lifestyles(self) -> list[str]:
+        x = self.data["user"].get("selected_descriptors", [])
 
-# def has_blacklisted_interests(d):
-#     interests = d(className="android.view.ViewGroup", resourceId="com.tinder:id/chip_group_alibis")
-#     for l in blacklisted_interests:
-#         if interests.child(text=l, className="android.view.View").wait.exists(timeout=1):
-#             print(f"Rejected because {l}")
-#             return True
-#     return False
+        ret = {}
 
-# def has_blacklisted_intent(d):
-#     for l in blacklisted_intent:
-#         if d(text=l, className="android.widget.TextView", resourceId="com.tinder:id/relationship_intent_bottom_prompt").wait.exists(timeout=1):
-#             print(f"Rejected because {l}")
-#             return True
-#     return False
+        for lifestyle in x:
+            name = lifestyle["name"]
+            choices = lifestyle["choice_selections"]
+            choices = [c["name"] for c in choices]
 
-# def has_blacklisted_bio(d):
-#     if not d(className="android.widget.TextView", resourceId="com.tinder:id/text_view_bio").wait.exists(timeout=100):
-#         return False
-#     bio = d(className="android.widget.TextView", resourceId="com.tinder:id/text_view_bio").info["text"]
-#     #print(bio)
-#     bio = bio.lower()
-#     for l in blacklisted_bio:
-#         if l in bio:
-#             print(f"rejected because '{l}' in bio")
-#             return True
-
-#     return False
-
-# def has_blacklisted_job(d):
-#     if d(resourceId="com.tinder:id/profile_info_job", className="android.widget.TextView").wait.exists(timeout=100):
-#         job = d(resourceId="com.tinder:id/profile_info_job", className="android.widget.TextView").info["text"].lower()
-#         for l in blacklisted_jobs:
-#             if l in job:
-#                 print(f"rejected because {l}")
-#                 return True
-#     return False
-
-# def reject(d):
-#     d(className="android.widget.FrameLayout", resourceId="com.tinder:id/gamepad_pass").click()
-# def accept(d):
-#     d(className="android.widget.FrameLayout", resourceId="com.tinder:id/gamepad_like").click()
-
-# def main():
-#     client = AdbClient(host="127.0.0.1", port=5037)
-#     devices = client.devices()
-#     devices = [d.serial for d in devices]
-#     if len(devices) > 1 :
-#         questions = [
-#         inquirer.List('device',
-#                     message="What device do you wanna connect to?",
-#                     choices=devices,
-#                 ),
-#         ]
-#         answers = inquirer.prompt(questions)
-#         device = answers['device']
-#     else:
-#         device = devices[0]
-#     #device = await client.device(device)
-#     d = u2.connect(device)
-#     d.app_start('com.example.hello_world')
-
-#     while True:
-
-#         click_more_info(d)
-
-#         #bio = get_bio(d)
-#         #print("Bio: ", bio)
-
-#         if has_blacklisted_interests(d) or has_blacklisted_lifestyle(d) or has_blacklisted_intent(d) or has_blacklisted_bio(d) or has_blacklisted_job(d):
-#             reject(d)
-#             continue
+            ret[name] = choices
+        return [i for j in ret.values() for i in j]
 
 
-#         #inquirer.prompt([inquirer.Text('input', message='make a manual decision and press enter to continue')])
-#         print('.', end='')
-#         x = input("press enter to continue")
-#         #time.sleep(1)
+    def job_titles(self) -> str:
+        x = self.data["user"]["jobs"]
+        return [i["title"]["name"] for i in x]
+
+    def intent(self) -> list[str]:
+        x = self.data["user"].get("relationship_intent", {}).get("body_text", "")
+        return x
+
+    def __repr__(self) -> str:
+        x = {
+            "name": self.name(),
+            "bio": self.bio(),
+            "interests": self.interests(),
+            "job_titles": self.job_titles(),
+            "lifestyles": self.lifestyles(),
+            "intent": self.intent(),
+        }
+        return json.dumps(x, indent=2, ensure_ascii=False)
+    
+    def check_job(self, blacklist: list[str]):
+        blacklist = {b.lower() for b in blacklist}
+        jobs = self.job_titles()
+        jobs = (j.lower() for j in jobs)
+        for j in jobs:
+            for b in blacklist:
+                if b in j:
+                    return True
+        return False
+                
+
+    def check_bio(self, blacklist: list[str]):
+        blacklist = {b.lower() for b in blacklist}
+        bio = self.bio().lower()
+        for b in blacklist:
+            if b in bio:
+                return True
+        return False
+
+    def check_interests(self, blacklist: list[str]):
+        blacklist = {b.lower() for b in blacklist}
+        interests = self.interests()
+        interests = (i.lower() for i in interests)
+        for i in interests:
+            if i in blacklist:
+                return True
+        return False
+
+    def check_intent(self, blacklist: list[str]):
+        blacklist = {b.lower() for b in blacklist}
+        if self.intent().lower() in blacklist:
+            return True
+        return False
+
+    def check_lifestyle(self, blacklist: list[str]):
+        blacklist = {b.lower() for b in blacklist}
+        lifestyles = self.lifestyles()
+        lifestyles = (l.lower() for l in lifestyles)
+        for l in lifestyles:
+            if l in blacklist:
+                return True
+        return False
+
+class ApiClient:
+    def __init__(self, token: str):
+        user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/109.0.5414.112 Mobile/15E148 Safari/604.1"
+        self.headers = {
+            "User-agent": user_agent,
+            "Content-Type": "application/json",
+            "X-Auth-Token": token,
+        }
+        self.baseurl = "https://api.gotinder.com"
+
+    def get_recs(self) -> dict:
+        # res = requests.get(self.baseurl+"/v2/recs/core?locale=en", headers=self.headers)
+        # return res.json()
+        raise Exception("Unimplemented")
+
+    def recs_to_users(self, recs: dict) -> list[User]:
+        return [User(r) for r in recs["data"]["results"]]
 
 
 class Swiper:
     def __init__(self, device: str):
-        self.d = u2.connect(device)
+        self.client = ApiClient("token")    
         print(f"Connected to {device}")
         # self.d.app_start("com.tinder"
         self.load_config()
         self.d.open_url("tinder://")
 
-        self.swipe()
+        while True:
+            try:
+                self.swipe()
+            except Exception as e:
+                print(e, file=sys.stderr)
 
     def load_config(self):
         with open("blacklist.yml") as f:
@@ -211,44 +228,23 @@ class Swiper:
 
         return False
 
-    def check_job(self):
-        if self.d(
-            resourceId="com.tinder:id/profile_info_job",
-            className="android.widget.TextView",
-        ).exists:
-            job = (
-                self.d(
-                    resourceId="com.tinder:id/profile_info_job",
-                    className="android.widget.TextView",
-                )
-                .info["text"]
-                .lower()
-            )
-            for l in self.blacklist["jobs"]["list"]:
-                if l in job:
-                    self.reject(f"{l} in job: {job}")
-                    return True
-        return False
+   
+
+       
 
 
 if __name__ == "__main__":
-    client = AdbClient(host="127.0.0.1", port=5037)
-    devices = client.devices()
-    devices = [d.serial for d in devices]
-    if len(devices) < 1:
-        print("No devices found")
-        exit()
-    if len(devices) > 1:
-        questions = [
-            inquirer.List(
-                "device",
-                message="What device do you wanna connect to?",
-                choices=devices,
-            ),
-        ]
-        answers = inquirer.prompt(questions)
-        device = answers["device"]
-    else:
-        device = devices[0]
+    import os
 
-    s = Swiper(device)
+    token = os.environ.get("TOKEN")
+    if not token:
+        raise Exception("no token")
+    a = ApiClient(token)
+
+    x = a.get_recs()
+    x = json.dumps(x, indent=4)
+
+    with open("testdata.json", "w") as f:
+        f.write(x)
+
+    print(x)
---
+++ tests/test_main.py
@@ -0,0 +1,30 @@
+import pytest
+from swiper.main import ApiClient
+import json
+
+
+@pytest.fixture(autouse=True)
+def mocked_recs(mocker):
+    j = None
+    with open("tests/testdata/recs.json") as f:
+        j = json.load(f)
+    mocker.patch.object(ApiClient, "get_recs", return_value=j)
+
+
+# @pytest.fixture(scope="session", autouse=True)
+# def setup():
+#     print("setup")
+#     yield
+#     print("teardown")
+
+
+def test_filters():
+    a = ApiClient("")
+    x = a.get_recs()
+    b = a.recs_to_users(x)
+    for u in b:
+        print(u.bio(), u.check_bio(["vaxx"]))
+        print(u.intent(), u.check_intent(["new friends"]))
+        print(u.interests(), u.check_interests(["cat lover"]))
+        print(u.job_titles(), u.check_job(["dog"]))
+        print(u.lifestyles(), u.check_lifestyle(["dog"]))
